802e9717736be791229f8430d5e97b56e230ddfc
hh120901 <70877597+hh120901@users.noreply.github.com>
2022-08-13 16:24:08 +0700

    learning

--- appr/src/App.js
+++ appr/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useContext } from "react";
+const ExampleContext = React.createContext();
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ExampleContext.Provider value={{ size: 20 }}>
+      <div className='App'>
+        <ChildComponent />
+      </div>
+    </ExampleContext.Provider>
   );
-}
+};
+
+const ChildComponent = () => {
+  const { size } = React.useContext(ExampleContext);
+
+  return <p style={{ size }}>This text is {size}</p>;
+};
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>hello World</h1>
+//     </div>
+//   );
+// }
 
 export default App;
