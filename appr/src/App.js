20831152f71e6fc5d34ea1d358e111af86d0b979
hh120901 <70877597+hh120901@users.noreply.github.com>
2022-08-14 13:04:10 +0700

    SearchMoviesApp
    using useEffect and useState

--- appr/src/App.js
+++ appr/src/App.js
@@ -1,30 +1,61 @@
-import React from "react";
-import { useContext } from "react";
-const ExampleContext = React.createContext();
+import React from 'react';
+import { useEffect,useState } from 'react';
+import './App.css';
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
 
-const App = () => {
+//f4651921 : api key
+const API_URL = 'https://www.omdbapi.com?apikey=f4651921';
+function App() {
+    //state movie
+    const [movies, setMovies] = useState([]);
+    // state search
+    const [searchTerm,setSearchTerm] = useState('');
+
+    const SearchMovie = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        //set movies = results of search
+        setMovies(data.Search);
+    }
+    useEffect(() => {
+      SearchMovie('batman');
+    }, [])
+    
   return (
-    <ExampleContext.Provider value={{ size: 20 }}>
-      <div className='App'>
-        <ChildComponent />
+    <div className="app">
+      <h1>Films Search</h1>
+      <div className='search'>
+        <input 
+            placeholder='Search for movies'
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
+        />
+        <img 
+            src={SearchIcon} 
+            alt="search"
+            onClick={()=>SearchMovie(searchTerm)}
+        />
       </div>
-    </ExampleContext.Provider>
+      
+      {
+        movies?.length >0
+        ? (
+            <div className='container'> 
+                {movies.map((movie,index)=>(
+                    <MovieCard movie={movie} key={index}/>
+                ))}
+            </div>
+        )
+        : (
+            <div className='empty'> 
+                <h2>No movies found</h2>
+            </div>
+        )
+      }
+      
+    </div>
   );
-};
-
-const ChildComponent = () => {
-  const { size } = React.useContext(ExampleContext);
-
-  return <p style={{ size }}>This text is {size}</p>;
-};
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>hello World</h1>
-//     </div>
-//   );
-// }
+}
 
 export default App;
